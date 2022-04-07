b60e2df5c0afd8473051ee77fad34136dfb274cf
Liv0130 <yjjhsh27@gmail.com>
2022-04-07 23:11:30 +0900

    Completed backend works

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -6,6 +6,7 @@ import mongoose from 'mongoose';
 
 import api from './api';
 import createFakeData from './createFakeData';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 // 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
 const { PORT, MONGO_URI } = process.env;
@@ -31,6 +32,7 @@ router.use('/api', api.routes()); // api route
 
 // apply bodyParser before using router
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 // apply router to app instance
 app.use(router.routes()).use(router.allowedMethods());
