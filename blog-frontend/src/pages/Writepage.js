1e0fbed0d73fc23a2db295d8fa8b4c1daa1589ce
Liv0130 <yjjhsh27@gmail.com>
2022-04-17 21:01:29 +0900

    Applied react-helmet-async

--- blog-frontend/src/pages/Writepage.js
+++ blog-frontend/src/pages/Writepage.js
@@ -2,10 +2,14 @@ import EditorContainer from '../containers/write/EditorContainer';
 import TagBoxContainer from '../containers/write/TagBoxContainer';
 import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 import Responsive from '../components/common/Responsive';
+import { Helmet } from 'react-helmet-async';
 
 const WritePage = () => {
   return (
     <Responsive>
+      <Helmet>
+        <title>Write a post-REACTERS</title>
+      </Helmet>
       <EditorContainer />
       <TagBoxContainer />
       <WriteActionButtonsContainer />
